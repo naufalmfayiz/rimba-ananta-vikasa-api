a32ad175dd22943011315bfb1e7a56b8fa66c879
naufalmfayiz <fayiznaufal@gmail.com>
2025-01-12 12:33:18 +0700

    refactor: change format for logger

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# rimba-ananta-vikasa-api
+# RAV API
+
+API sederhana untuk manajemen data pengguna menggunakan Node.js, Express, dan Sequelize dengan PostgreSQL sebagai database.
+
+---
+
+## **Fitur**
+
+- CRUD (Create, Read, Update, Delete) untuk entitas **User**.
+- Validasi input menggunakan middleware.
+- Logging request ke file log.
+- Pengujian endpoint menggunakan Jest.
+- Terintegrasi dengan database PostgreSQL menggunakan Sequelize ORM.
+
+---
+
+## **Persyaratan**
+
+- **Node.js**: >= 14.x
+- **PostgreSQL**: >= 12.x
+- **npm** atau **yarn**
+
+---
+
+## **Instalasi dan Menjalankan Aplikasi**
+
+### **1. Clone Repository**
+
+```bash
+git clone <repository-url>
+cd <repository-folder>
+```
+
+### **2. Install Dependencies**
+
+```bash
+npm install
+```
+
+### **3. Buat Database**
+
+```bash
+npx sequelize-cli db:create
+npx sequelize-cli db:create --env test
+```
+
+### **4. Migrasi**
+
+```bash
+npx sequelize-cli db:migrate
+npx sequelize-cli db:migrate --env test
+```
+
+### **5. Jalankan Server**
+
+```bash
+npm run dev
+```
--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,12 +1,36 @@
 const winston = require("winston");
+const path = require("path");
+const fs = require("fs");
+
+const logsDir = path.join(__dirname, "..", "logs");
+if (!fs.existsSync(logsDir)) {
+  fs.mkdirSync(logsDir);
+}
+
+const getCurrentDate = () => {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const day = String(now.getDate()).padStart(2, "0");
+  return `${day}-${month}-${year}`;
+};
+
+const logFilename = path.join(logsDir, `${getCurrentDate()}-requests.log`);
 
 const logger = winston.createLogger({
-  format: winston.format.json(),
-  transports: [new winston.transports.File({ filename: "requests.log" })],
+  format: winston.format.combine(
+    winston.format.printf(({ level, message }) => {
+      return `${message}`;
+    })
+  ),
+  transports: [new winston.transports.File({ filename: logFilename })],
 });
 
 const logMiddleware = (req, res, next) => {
-  logger.info(`${req.method} ${req.url}`);
+  const now = new Date();
+  const time = now.toTimeString().split(" ")[0];
+
+  logger.info(`[${time}] ${req.method} ${req.url}`);
   next();
 };
 
